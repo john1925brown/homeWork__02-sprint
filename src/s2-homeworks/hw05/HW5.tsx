4aad1c380002414f31e37253646fec17e1d364b0
Dzmitry Yarasheuski <materik08082020@gmail.com>
2025-01-17 03:23:02 +0300

    hw 03 sptint 02 lesson

---
+++ src/s2-homeworks/hw05/HW5.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { HashRouter } from 'react-router-dom'
+import { Layout } from './layout/Layout'
+import Pages from './Pages'
+
+/*
+* 3 - застилизовать хэдэр и сайдбар в соответствии с дизайном
+* */
+
+function HW5() {
+    return (
+        <HashRouter>
+            {/*в gh-pages лучше работает HashRouter, с BrowserRouter скорее всего не пройдёт тест*/}
+            <Layout>
+                <Pages />
+            </Layout>
+        </HashRouter>
+    )
+}
+
+export default HW5
