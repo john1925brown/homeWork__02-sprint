0739c56b59ff123a65a4b9e4404c4c3dd64940be
Dzmitry Yarasheuski <materik08082020@gmail.com>
2025-02-03 21:33:21 +0300

    hw__03__03 logic without radio styles

--- src/s2-homeworks/hw05/HW5.tsx
+++ src/s2-homeworks/hw05/HW5.tsx
@@ -3,9 +3,7 @@ import { HashRouter } from 'react-router-dom'
 import { Layout } from './layout/Layout'
 import Pages from './Pages'
 
-/*
-* 3 - застилизовать хэдэр и сайдбар в соответствии с дизайном
-* */
+
 
 function HW5() {
     return (
