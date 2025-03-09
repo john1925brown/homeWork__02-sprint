01ae91ea602c7f23e2aeb61c4186d3daf8dee365
Dzmitry Yarasheuski <materik08082020@gmail.com>
2025-03-09 13:16:24 +0300

    hw__04__02

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
-// import HW10 from '../../hw10/HW10'
+import React from 'react';
+import HW10 from '../../hw10/HW10';
+import { Provider } from 'react-redux';
+import store from '../../hw10/bll/store';
 // import HW11 from '../../hw11/HW11'
 // import HW12 from '../../hw12/HW12'
 // import HW13 from '../../hw13/HW13'
@@ -7,16 +9,18 @@ import React from 'react'
 // import HW15 from '../../hw15/HW15'
 
 function JuniorPlus() {
-    return (
-        <div id={'hw5-page-junior-plus'}>
-            {/*<HW10 />*/}
-            {/*<HW11 />*/}
-            {/*<HW12 />*/}
-            {/*<HW13 />*/}
-            {/*<HW14 />*/}
-            {/*<HW15 />*/}
-        </div>
-    )
+  return (
+    <div id={'hw5-page-junior-plus'}>
+      <Provider store={store}>
+        <HW10 />
+        {/*<HW11 />*/}
+        {/*<HW12 />*/}
+        {/*<HW13 />*/}
+        {/*<HW14 />*/}
+        {/*<HW15 />*/}
+      </Provider>
+    </div>
+  );
 }
 
-export default JuniorPlus
+export default JuniorPlus;
